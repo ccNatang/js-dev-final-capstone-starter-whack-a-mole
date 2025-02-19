aa4e1c3ad643db505d83f77bbdb9f8a44ce54479
ccNatang <ncole1128@gmail.com>
2025-02-19 17:16:53 -0600

    Update index.js
    Added audio song

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ let lastHole = 0;
 let points = 0;
 let difficulty = "easy";
 
+const audioHit = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/hit.mp3?raw=true");
+const song = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/molesong.mp3?raw=true");
+
 /**
  * Generates a random integer within a range.
  *
@@ -42,7 +45,22 @@ function setDifficulty(){
 }
 setDifficulty()
 
+function playAudio(audioObject) {
+  audioObject.play();
+}
 
+function loopAudio(audioObject) {
+  audioObject.loop = true;
+  playAudio(audioObject);
+}
+
+function stopAudio(audioObject) {
+  audioObject.pause();
+}
+
+function play(){
+  playAudio(song);
+}
 
 /**
  * Sets the time delay given a difficulty parameter.
